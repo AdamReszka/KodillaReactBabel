01fcc4592c6a3779930c2a0319212615b33ac112
Michał Gacek <gacek.michael@outlook.com>
2017-11-14 00:56:43 +0100

    Fixes

--- components/Contact.js
+++ components/Contact.js
@@ -7,17 +7,16 @@ var Contact = React.createClass({
     var contactSex = '';
     var contactName = this.props.item.firstName;
 
-
-
-      var stringLength = contactName.length;
-      var lastChar = contactName.charAt(stringLength - 1);
-      if (lastChar == 'a'){
-        contactSex = 'girl';
-      } else {
-        contactSex = 'boy';
-      }
+    var stringLength = contactName.length;
+    var lastChar = contactName.charAt(stringLength - 1);
+    
+    if (lastChar == 'a') {
+      contactSex = 'girl';
+    } else {
+      contactSex = 'boy';
+    }
+    
     return (
-
       <li className={'contactItem'}>
         <div className={'imageCont'}>
           <img className={'contactImage'} src={'images/' +contactSex+ '.png'}/>
@@ -29,22 +28,6 @@ var Contact = React.createClass({
           {this.props.item.email}
         </a>
       </li>
-
-  /*    React.createElement('li', {className: 'contactItem'},
-        React.createElement('div', {className: 'imageCont'},
-          React.createElement('img', {
-            className: 'contactImage',
-            src: 'images/' +contactSex+ '.png'
-          }),
-        ),
-
-        React.createElement('p', {className: 'contactLabel'}, this.props.item.firstName + ' ' + this.props.item.lastName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + ),
-        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email}, this.props.item.email)
-      )
-
-*/
-
     );
   },
 });
